0ec7ecd34e39389546c0e9ad3ae559d686eca018
Matthew Whitby <34402877+Whitbyfish@users.noreply.github.com>
2019-04-07 16:54:55 +0100

    Update translationScript.js

--- translations/js/translationScript.js
+++ translations/js/translationScript.js
@@ -1,19 +1,28 @@
-function openTab(evt, tabName) {
-    var i, tabcontent, tablinks;
+function openTab(evt,tabName){
+    var i,tabcontent,tablinks;
   
     // Hide tabs
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
+    tabcontent=document.getElementsByClassName("tabcontent");
+    for(i=0;i<tabcontent.length;i++)
+      tabcontent[i].style.display="none";
   
     // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tabbtn");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+    tablinks=document.getElementsByClassName("tabbtn");
+    for(i=0;i<tablinks.length;i++)
+      tablinks[i].className=tablinks[i].className.replace(" active", "");
   
+    if(tabName=="hiragana")HideRomaji(tabName);
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
+    document.getElementById(tabName).style.display="block";
+    evt.currentTarget.className+=" active";
   } 
+
+  function HideRomaji(tabName){
+    var hiragana=document.getElementById('hiragana');
+    var childNodes=hiragana.childNodes[1].childNodes;
+    for(i=0;i<childNodes.length;i++){
+      if(childNodes[i].className=="line"){
+        console.log(childNodes[i].lastChild.firstChild.textContent);
+      }
+    }
+  }
