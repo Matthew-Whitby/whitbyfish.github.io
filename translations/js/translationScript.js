932b4615d60123e2cbf01ca734421033fa2485bf
Matthew Whitby <34402877+Whitbyfish@users.noreply.github.com>
2019-04-15 06:11:06 +0100

    Update translationScript.js

--- translations/js/translationScript.js
+++ translations/js/translationScript.js
@@ -1,30 +1,27 @@
-function openTab(evt, tabName) {
-  let i, tabcontent, tablinks;
-  // Hide tabs
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++)
-    tabcontent[i].style.display = "none";
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tabbtn");
-  for (i = 0; i < tablinks.length; i++)
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  if (tabName == "hiragana") HideRomaji(tabName);
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(tabName).style.display = "block";
-  evt.currentTarget.className += " active";
+function openTab(evt,tabName) {
+  let i,tabcontent,tablinks;
+  tabcontent=document.getElementsByClassName("tabcontent");
+  for(i=0;i<tabcontent.length;i++)
+    tabcontent[i].style.display="none";
+  tablinks=document.getElementsByClassName("tabbtn");
+  for(i=0;i<tablinks.length;i++)
+    tablinks[i].className=tablinks[i].className.replace(" active","");
+  if(tabName=="hiragana")HideRomaji(tabName);
+  document.getElementById(tabName).style.display="block";
+  evt.currentTarget.className+=" active";
 }
 
 function HideRomaji(tabName) {
-  let hiragana = document.getElementById(tabName + "lyrics");
-  let childNodes = hiragana.children;
-  for (i = 0; i < childNodes.length; i++) {
-    let node = FindNodeByClass(childNodes[i].children, "romaji", true);
-    if (node.parentElement.children.length == 1) {
-      node.style.opacity = "0";
-      let button = document.createElement("BUTTON");
-      button.innerHTML = "Toggle Visibility";
-      button.className = "visible";
-      button.addEventListener("click", ToggleVisibility, this);
+  let hiragana=document.getElementById(tabName+"lyrics");
+  let childNodes=hiragana.children;
+  for(i=0;i<childNodes.length;i++){
+    let node=FindNodeByClass(childNodes[i].children,"romaji",true);
+    if(node.parentElement.children.length==1){
+      node.style.opacity="0";
+      let button=document.createElement("BUTTON");
+      button.innerHTML="Toggle Visibility";
+      button.className="visible";
+      button.addEventListener("click",ToggleVisibility,this);
       node.parentElement.appendChild(button);
     }
   }
