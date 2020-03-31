ec5010e0028c36929cdfed254fc05ea34e175ad1
Matthew Whitby <34402877+Whitbyfish@users.noreply.github.com>
2020-03-31 17:11:55 +0100

    toggle sidenav added

--- translations/js/wasutablogcontents.js
+++ translations/js/wasutablogcontents.js
@@ -19,3 +19,12 @@ function MoveTopBtn(val){
          break;
    }
 }
+function ToggleSideNav(){
+   let sideNav=document.getElementById("sideNav");
+   let childNodes=sideNav.children;
+   for(var i=0;i<childNodes.length;i++){
+      if(childNodes[i].id!="sideNavToggle"){
+         childNodes[i].style.display=(childNodes[i].style.display=="none")?"block":"none";
+      }
+   }
+}
