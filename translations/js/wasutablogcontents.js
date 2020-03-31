28a5ade616d3670ddd04bdaf450808b96c284f47
Matthew Whitby <34402877+Whitbyfish@users.noreply.github.com>
2020-03-31 17:17:13 +0100

    a

--- translations/js/wasutablogcontents.js
+++ translations/js/wasutablogcontents.js
@@ -1,24 +1,3 @@
-function MoveTopBtn(val){
-   let nav=document.getElementById("sideNav");
-   switch(val){
-      case "off":
-         nav.style.display="none";
-         break;
-      case "top":
-         nav.style.display="block";
-         nav.style.top="5%";
-         break;
-      case "bottom":
-         nav.style.display="block";
-         let newtop=window.height-nav.style.height;
-         nav.style.top=newtop+"px";
-         break;
-      case "middle":
-         nav.style.display="block";
-         nav.style.top="25%";
-         break;
-   }
-}
 function ToggleSideNav(){
    let sideNav=document.getElementById("sideNav");
    let childNodes=sideNav.children;
