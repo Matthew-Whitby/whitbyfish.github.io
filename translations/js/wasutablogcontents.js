40958bc061d6f57d8e8875883b8cdce3ccfb3e14
Matthew Whitby <34402877+Whitbyfish@users.noreply.github.com>
2020-03-31 18:44:58 +0100

    added sorting

--- translations/js/wasutablogcontents.js
+++ translations/js/wasutablogcontents.js
@@ -1,9 +1,37 @@
 function ToggleSideNav(){
    let sideNav=document.getElementById("sideNav");
    let childNodes=sideNav.children;
-   for(var i=0;i<childNodes.length;i++){
+   let i;
+   for(i=0;i<childNodes.length;i++){
       if(childNodes[i].id!="sideNavToggle"){
          childNodes[i].style.display=(childNodes[i].style.display=="none")?"block":"none";
       }
+   }
+}
+function SelectYear(val){
+   let years=document.getElementsByClassName("year");
+   let i;
+   for(i=0;i<years.length;i++){
+      if(val=="all")years[i].style.display="initial";
+      else if(years[i].id==val)years[i].style.display="initial";
+      else years[i].style.display="none";
+   }
+}
+function SelectMonth(val){
+   let months=document.getElementsByClassName("month");
+   let i;
+   for(i=0;i<months.length;i++){
+      let classes=months[i].classList;
+      let j,monthClass;
+      let monthlist=["january","february","march","april","may","june","july","august","september","october","november","december"];
+      for(j=0;j<classes.length;j++){
+         let z;
+         for(z=0;z<monthlist.length;z++){
+            if(classes[j]==monthlist[z])monthClass=classes[j];
+         }
+      }
+      if(val=="all")months[i].style.display="initial";
+      else if(monthClass==val)months[i].style.display="initial";
+      else months[i].style.display="none";
    }
 }
