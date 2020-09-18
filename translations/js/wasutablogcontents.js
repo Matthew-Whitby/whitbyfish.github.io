87083bf2f30981cf428e7ab7a88a6a2f43109b99
Matthew Whitby <matj1@aol.com>
2020-09-18 12:47:49 +0100

    Update wasutablogcontents.js

--- translations/js/wasutablogcontents.js
+++ translations/js/wasutablogcontents.js
@@ -38,12 +38,12 @@ function ResetSelection(l,c){
    }return [false];
 }
 function SelectYear(val){
-   let years=GetCL("year"),i;
+   let years=GetCN("year"),i;
    for(i=0;i<years.length;i++)
       years[i].style.display=(val=="all"||years[i].id==val)?"initial":"none";
 }
 function SelectMonth(val){
-   let months=GetCL("month"),i;
+   let months=GetCN("month"),i;
    for(i=0;i<months.length;i++){
       let classes=months[i].classList,j,monthClass;
       for(j=0;j<classes.length;j++)
@@ -52,17 +52,17 @@ function SelectMonth(val){
    }
 }
 function SelectNotOnSite(){
-   let done=GetCL("done"),i;
+   let done=GetCN("done"),i;
    for(i=0;i<done.length;i++)done[i].style.display=done[i].classList.contains("notsite")?"initial":"none";
-   let notdone=[...GetCL("notdone"),...GetCL("inprogress")];
+   let notdone=[...GetCN("notdone"),...GetCN("inprogress")];
    for(i=0;i<notdone.length;i++)notdone[i].style.display="none";
 }
 function SelectMember(val){
-   let hazukiPosts=GetCL("hazuki");
-   let nanasePosts=GetCL("nanase");
-   let miriPosts=GetCL("miri");
-   let ririkaPosts=GetCL("ririka");
-   let rukaPosts=GetCL("ruka");
+   let hazukiPosts=GetCN("hazuki");
+   let nanasePosts=GetCN("nanase");
+   let miriPosts=GetCN("miri");
+   let ririkaPosts=GetCN("ririka");
+   let rukaPosts=GetCN("ruka");
    switch(val){
       case"all":
          ToggleDisplayMem([...hazukiPosts,...nanasePosts,...miriPosts,...ririkaPosts,...rukaPosts],true);
@@ -147,14 +147,14 @@ function CheckParameters(){
 }
 function BlogCount(){
    document.getElementById("totalCount").innerText="Total: "+TotalBlogs();
-   document.getElementById("hazukiCount").innerText="Hazuki: "+GetCL("hazuki").length;
-   document.getElementById("nanaseCount").innerText="Nanase: "+GetCL("nanase").length;
-   document.getElementById("miriCount").innerText="Miri: "+GetCL("miri").length;
-   document.getElementById("ririkaCount").innerText="Ririka: "+GetCL("ririka").length;
-   document.getElementById("rukaCount").innerText="Ruka: "+GetCL("ruka").length;
+   document.getElementById("hazukiCount").innerText="Hazuki: "+GetCN("hazuki").length;
+   document.getElementById("nanaseCount").innerText="Nanase: "+GetCN("nanase").length;
+   document.getElementById("miriCount").innerText="Miri: "+GetCN("miri").length;
+   document.getElementById("ririkaCount").innerText="Ririka: "+GetCN("ririka").length;
+   document.getElementById("rukaCount").innerText="Ruka: "+GetCN("ruka").length;
 }
-function CalculatePercentage(){document.getElementById("percentage").innerText=(GetCL("done").length/5244)*100+"% Complete";}
-function TotalBlogs(){return[...GetCL("hazuki"),...GetCL("nanase"),...GetCL("miri"),...GetCL("ririka"),...GetCL("ruka")].length}
+function CalculatePercentage(){document.getElementById("percentage").innerText=(GetCN("done").length/5244)*100+"% Complete";}
+function TotalBlogs(){return[...GetCN("hazuki"),...GetCN("nanase"),...GetCN("miri"),...GetCN("ririka"),...GetCN("ruka")].length}
 function ScrollToTop(){window.scroll({top:0,left:0,behavior:'smooth'});var v=document.getElementsByClassName("isCurrent");if(v.length>0)v[0].classList.remove("isCurrent");}
 function ScrollToYear(year,clicked){var e=document.getElementById(year);e.scrollIntoView({behavior:'smooth'});var v=document.getElementsByClassName("isCurrent");if(v.length>0)v[0].classList.remove("isCurrent");clicked.classList.add("isCurrent");}
 function ChangeDisplay(e){var l=document.getElementsByClassName("done"),n=document.getElementsByClassName("inprogress"),s=document.getElementsByClassName("notdone");switch(e){case"all":ToggleDisplay(l,!0),ToggleDisplay(n,!0),ToggleDisplay(s,!0);break;case"complete":ToggleDisplay(l,!0),ToggleDisplay(n,!1),ToggleDisplay(s,!1);break;case"inprogress":ToggleDisplay(l,!1),ToggleDisplay(n,!0),ToggleDisplay(s,!1);break;case"notdone":ToggleDisplay(l,!1),ToggleDisplay(n,!1),ToggleDisplay(s,!0)}}
@@ -162,5 +162,5 @@ function ToggleDisplay(e,l){for(z=0;z<e.length;z++)l?e[z].parentElement.parentEl
 function MonthToNum(month){switch(month){case"january":return 1;case"february":return 2;case"march":return 3;case"april":return 4;case"may":return 5;case"june":return 6;case"july":return 7;case"august":return 8;case"september":return 9;case"october":return 10;case"november":return 11;case"december":return 12}}
 function NumToMonth(month){switch(month){case 13:case 1:return"january";case 2:return"february";case 3:return"march";case 4:return"april";case 5:return"may";case 6:return"june";case 7:return"july";case 8:return"august";case 9:return"september";case 10:return"october";case 11:return"november";case 12:return"december";}}
 function IsMonth(n){n=n.toLowerCase();return(monthlist.includes(n))?!0:!1;}
-function GetCL(n){return document.getElementsByClassName(n);}
+function GetCN(n){return document.getElementsByClassName(n);}
 function ToggleDisplayMem(e,l){for(z=0;z<e.length;z++)l?e[z].parentElement.classList.contains("mHidden")&&e[z].parentElement.classList.remove("mHidden"):e[z].parentElement.classList.contains("mHidden")||e[z].parentElement.classList.add("mHidden"),CheckTableMem(e[z])}function CheckTableMem(e){var l=e.parentElement.parentElement.parentElement;DetectVisibleElementMem(l)?l.classList.contains("mHidden")&&l.classList.remove("mHidden"):l.classList.contains("mHidden")||l.classList.add("mHidden")}function DetectVisibleElementMem(e){switch(e.tagName){case"TR":return"TH"!=e.childNodes[0].tagName&&!e.classList.contains("mHidden");default:if(null!=e&&null!=e.childNodes&&e.childNodes.length>0){for(var l=0;l<e.childNodes.length;l++)if(DetectVisibleElementMem(e.childNodes[l]))return!0;return!1}}}
